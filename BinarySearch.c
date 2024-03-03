4e31a88e922e2efa0c7a085d91ae39eba9fecbe7
Md Alamin <108076684+alamin513@users.noreply.github.com>
2024-03-03 21:48:59 +0600

    Add files via upload

---
+++ BinarySearch.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int A[]={5,7,9,12,15,20,24,27,33,40,50,65,72};
+    int data=33;
+    int l,r,mid;
+    l=0;
+    r=12;
+
+    while(l<=r)
+    {
+        mid=(l+r)/2;
+        if(A[mid]==data)
+        {
+           printf("The searching data is %d\n",mid);
+           return 0;
+        }
+        else if(A[mid]<data)
+        {
+            l=mid+1;
+        }
+        else
+        {
+            r=mid-1;
+        }
+    }
+    printf("Data is not found");
+    return 0;
+}
